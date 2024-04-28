6d049ff3ca5ce779256d4295108d09145a65843d
Vadym Hrashchenko <vhrashch@cisco.com>
2024-04-28 14:33:51 +0300

    added go test generator

--- .config/nvim/lua/hrvadl/plugins/treesitter.lua
+++ .config/nvim/lua/hrvadl/plugins/treesitter.lua
@@ -1,18 +1,18 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-		'nvim-treesitter/playground'
-
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/playground",
 	},
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "lua", "javascript", "html", "css", "typescript", "go" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end
+		configs.setup({
+			ensure_installed = { "lua", "javascript", "html", "css", "typescript", "go" },
+			sync_install = false,
+			disable = { "yaml" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
