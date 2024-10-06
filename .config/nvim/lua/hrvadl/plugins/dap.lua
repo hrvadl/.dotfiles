6c96e24b077e9e6e76dd44f9112fe9afafb07f7c
hrvadl <hrvadleo@gmail.com>
2024-10-06 10:27:17 +0300

    installed mason dap nvim to ensure that delve is 100% installed

--- .config/nvim/lua/hrvadl/plugins/dap.lua
+++ .config/nvim/lua/hrvadl/plugins/dap.lua
@@ -29,6 +29,18 @@ return {
     end,
   },
   {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "delve" },
+      })
+    end,
+  },
+  {
     "stevearc/overseer.nvim",
     opts = {},
     config = function()
