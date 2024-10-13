88a7741770ba8e4feb7ead4bd4a0b9732d647465
hrvadl <hrvadleo@gmail.com>
2024-10-13 19:55:08 +0300

    removed conflicts with gci (configured goimports)

--- .config/nvim/lua/hrvadl/plugins/lsp.lua
+++ .config/nvim/lua/hrvadl/plugins/lsp.lua
@@ -1,156 +1,159 @@
 return {
-	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-	{ "neovim/nvim-lspconfig" },
-	{ "L3MON4D3/LuaSnip" },
-	{
-		"williamboman/mason.nvim",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"jay-babu/mason-null-ls.nvim",
-		},
-		config = function()
-			local lsp_zero = require("lsp-zero")
-			lsp_zero.extend_lspconfig()
+  { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+  { "neovim/nvim-lspconfig" },
+  { "L3MON4D3/LuaSnip" },
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "jay-babu/mason-null-ls.nvim",
+    },
+    config = function()
+      local lsp_zero = require("lsp-zero")
+      lsp_zero.extend_lspconfig()
 
-			vim.cmd([[autocmd BufRead,BufNewFile Jenkinsfile set filetype=groovy]])
-			vim.cmd([[autocmd BufRead,BufNewFile *.tf set filetype=hcl]])
+      vim.cmd([[autocmd BufRead,BufNewFile Jenkinsfile set filetype=groovy]])
+      vim.cmd([[autocmd BufRead,BufNewFile *.tf set filetype=hcl]])
 
-			lsp_zero.on_attach(function(client, bufnr)
-				lsp_zero.default_keymaps({ buffer = bufnr })
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({ buffer = bufnr })
 
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, {})
-				vim.keymap.set("n", "gt", function()
-					vim.lsp.buf.type_definition()
-				end, {})
-				vim.keymap.set("n", "gi", function()
-					vim.lsp.buf.implementation()
-				end, {})
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, {})
-				vim.keymap.set("n", "<leader>ds", function()
-					vim.diagnostic.open_float()
-				end, {})
-				vim.keymap.set("n", "<leader>dj", function()
-					vim.diagnostic.goto_next()
-				end, {})
-				vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {})
-				vim.keymap.set("n", "<leader>dk", function()
-					vim.diagnostic.goto_prev()
-				end, {})
-				vim.keymap.set("n", "<leader>ca", function()
-					vim.lsp.buf.code_action()
-				end, {})
-				vim.keymap.set("n", "<leader>vrr", function()
-					vim.lsp.buf.references()
-				end, {})
-				vim.keymap.set("n", "<leader>r", function()
-					vim.lsp.buf.rename()
-				end, {})
-				vim.keymap.set("i", "<C-lh>", function()
-					vim.lsp.buf.signature_help()
-				end, {})
-			end)
+        vim.keymap.set("n", "gd", function()
+          vim.lsp.buf.definition()
+        end, {})
+        vim.keymap.set("n", "gt", function()
+          vim.lsp.buf.type_definition()
+        end, {})
+        vim.keymap.set("n", "gi", function()
+          vim.lsp.buf.implementation()
+        end, {})
+        vim.keymap.set("n", "K", function()
+          vim.lsp.buf.hover()
+        end, {})
+        vim.keymap.set("n", "<leader>ds", function()
+          vim.diagnostic.open_float()
+        end, {})
+        vim.keymap.set("n", "<leader>dj", function()
+          vim.diagnostic.goto_next()
+        end, {})
+        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {})
+        vim.keymap.set("n", "<leader>dk", function()
+          vim.diagnostic.goto_prev()
+        end, {})
+        vim.keymap.set("n", "<leader>ca", function()
+          vim.lsp.buf.code_action()
+        end, {})
+        vim.keymap.set("n", "<leader>vrr", function()
+          vim.lsp.buf.references()
+        end, {})
+        vim.keymap.set("n", "<leader>r", function()
+          vim.lsp.buf.rename()
+        end, {})
+        vim.keymap.set("i", "<C-lh>", function()
+          vim.lsp.buf.signature_help()
+        end, {})
+      end)
 
-			local function custom_rename_handler(err, result, ctx, config)
-				if err then
-					vim.notify("Rename failed: " .. err.message, vim.log.levels.ERROR)
-					return
-				end
+      local function custom_rename_handler(err, result, ctx, config)
+        if err then
+          vim.notify("Rename failed: " .. err.message, vim.log.levels.ERROR)
+          return
+        end
 
-				local client = vim.lsp.get_client_by_id(ctx.client_id)
-				vim.lsp.util.apply_workspace_edit(result, client.offset_encoding)
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        vim.lsp.util.apply_workspace_edit(result, client.offset_encoding)
 
-				-- Autosave and close affected buffers
-				for _, change in ipairs(result.changes or {}) do
-					for uri, edits in pairs(change) do
-						local bufnr = vim.uri_to_bufnr(uri)
-						if vim.api.nvim_buf_is_loaded(bufnr) then
-							vim.api.nvim_buf_call(bufnr, function()
-								vim.cmd("write")
-								vim.cmd("bdelete")
-							end)
-						end
-					end
-				end
+        -- Autosave and close affected buffers
+        for _, change in ipairs(result.changes or {}) do
+          for uri, edits in pairs(change) do
+            local bufnr = vim.uri_to_bufnr(uri)
+            if vim.api.nvim_buf_is_loaded(bufnr) then
+              vim.api.nvim_buf_call(bufnr, function()
+                vim.cmd("write")
+                vim.cmd("bdelete")
+              end)
+            end
+          end
+        end
 
-				vim.notify("Rename successful", vim.log.levels.INFO)
-			end
+        vim.notify("Rename successful", vim.log.levels.INFO)
+      end
 
-			-- Override the default rename handler
-			vim.lsp.handlers["textDocument/rename"] = vim.lsp.with(custom_rename_handler, {})
+      -- Override the default rename handler
+      vim.lsp.handlers["textDocument/rename"] = vim.lsp.with(custom_rename_handler, {})
 
-			local signs = {
-				Error = "",
-				Warn = "",
-				Hint = "",
-				Information = "",
-			}
+      local signs = {
+        Error = "",
+        Warn = "",
+        Hint = "",
+        Information = "",
+      }
 
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
 
-			require("mason").setup({})
+      require("mason").setup({})
 
-			require("mason-lspconfig").setup({
-				automatic_installation = true,
-				ensure_installed = {
-					-- Lua
-					"lua_ls",
-					-- JS/HTML/CSS
-					-- "stylelint_lsp",
-					-- GO
-					"gopls",
-					-- PROTO
-					"bufls",
-				},
-				handlers = {
-					lsp_zero.default_setup,
-				},
-			})
+      require("mason-lspconfig").setup({
+        automatic_installation = true,
+        ensure_installed = {
+          -- Lua
+          "lua_ls",
+          -- JS/HTML/CSS
+          -- "stylelint_lsp",
+          -- GO
+          "gopls",
+          -- PROTO
+          "bufls",
+        },
+        handlers = {
+          lsp_zero.default_setup,
+        },
+      })
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			lspconfig.gopls.setup({
-				settings = {
-					gopls = {
-						gofumpt = true,
-						staticcheck = true,
-					},
-				},
-			})
+      lspconfig.gopls.setup({
+        settings = {
+          gopls = {
+            gofumpt = true,
+            staticcheck = true,
+            buildFlags = { "-tags=all" },
+          },
+        },
+      })
 
-			require("mason-null-ls").setup({
-				ensure_installed = {
-					-- Groovy
-					-- Lua
-					"formatting.stylua",
-					-- SHELL
-					-- formatting.shellharden,
-					-- JS
-					"eslint_d",
-					"prettierd",
-					-- CSS
-					-- "stylelint",
-					-- GO
-					"golines",
-					"gofumpt",
-					"goimports-reviser",
-					"golangci-lint",
-					-- diagnostics.revive,
-					-- C
-					-- "clang-format",
-					-- "clang-d",
-					-- PROTO
-					"buf",
-					"protolint",
-				},
-				automatic_installation = false,
-			})
-		end,
-	},
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          -- Groovy
+          -- Lua
+          "formatting.stylua",
+          -- SHELL
+          -- formatting.shellharden,
+          -- JS
+          "eslint_d",
+          "prettierd",
+          -- CSS
+          -- "stylelint",
+          -- GO
+          "golines",
+          "gofumpt",
+          "goimports-reviser",
+          "goimports-reviser",
+          "goimports",
+          "golangci-lint",
+          -- diagnostics.revive,
+          -- C
+          -- "clang-format",
+          -- "clang-d",
+          -- PROTO
+          "buf",
+          "protolint",
+        },
+        automatic_installation = false,
+      })
+    end,
+  },
 }
--- .config/nvim/lua/hrvadl/plugins/none_ls.lua
+++ .config/nvim/lua/hrvadl/plugins/none_ls.lua
@@ -1,43 +1,44 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- Lua
-				null_ls.builtins.formatting.stylua,
-				-- JS
-				null_ls.builtins.formatting.prettierd,
-				-- GO
-				null_ls.builtins.diagnostics.staticcheck,
-				null_ls.builtins.formatting.golines,
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.formatting.goimports,
-				null_ls.builtins.diagnostics.golangci_lint,
-				-- PROTO
-				null_ls.builtins.formatting.buf,
-				null_ls.builtins.diagnostics.protolint,
-			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = false, timeout_ms = 4000 })
-						end,
-					})
-				end
-			end,
-		})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        -- Lua
+        null_ls.builtins.formatting.stylua,
+        -- JS
+        null_ls.builtins.formatting.prettierd,
+        -- GO
+        null_ls.builtins.diagnostics.staticcheck,
+        null_ls.builtins.formatting.golines,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.diagnostics.golangci_lint,
+        -- PROTO
+        null_ls.builtins.formatting.buf,
+        null_ls.builtins.diagnostics.protolint,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false, timeout_ms = 4000 })
+            end,
+          })
+        end
+      end,
+    })
 
-		vim.diagnostic.config({ underline = false, severity_sort = true })
-		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-			underline = false,
-		})
+    vim.diagnostic.config({ underline = false, severity_sort = true })
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      underline = false,
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
