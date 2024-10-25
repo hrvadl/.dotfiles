730fc64bb5f17b58456ed5fe69de0d20df644fc0
hrvadl <hrvadleo@gmail.com>
2024-10-25 19:47:55 +0300

    feat: finally found the RIGHT indent plugin

--- .config/nvim/lua/hrvadl/plugins/blankline.lua
+++ .config/nvim/lua/hrvadl/plugins/blankline.lua
@@ -1,20 +1,11 @@
 return {
   {
-    "lukas-reineke/indent-blankline.nvim",
-    dependencies = {},
-    main = "ibl",
+    "nvimdev/indentmini.nvim",
     config = function()
-      require("ibl").setup()
-    end,
-  },
-
-  {
-    "echasnovski/mini.indentscope",
-    version = "*",
-    config = function()
-      local indentscope = require("mini.indentscope")
-      indentscope.gen_animation.none()
-      indentscope.setup({ symbol = "|", debounce = 0 })
+      local c = require("nord.colors").palette
+      vim.api.nvim_set_hl(0, "IndentLine", { fg = c.polar_night.bright })
+      vim.api.nvim_set_hl(0, "IndentLineCurrent", { fg = c.frost.polar_water })
+      require("indentmini").setup()
     end,
   },
 }
